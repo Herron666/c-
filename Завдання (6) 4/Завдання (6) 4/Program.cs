00586caf13b0e95cbc9322b02306cc7ac448d457
Herron666 <74177897+Herron666@users.noreply.github.com>
2021-03-06 21:56:10 +0200

    Add files via upload

--- Завдання (6) 4/Завдання (6) 4/Program.cs
+++ Завдання (6) 4/Завдання (6) 4/Program.cs
@@ -6,7 +6,33 @@ namespace Завдання__6__4
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.Write("Введіть N = ");
+
+            int N = int.Parse(Console.ReadLine());
+
+            double x0 = 0;
+
+            double x1 = 9;
+
+            double x2 = 9;
+
+            double x4 = 0;
+
+            for (int i = 3; i < N + 3; i++)
+            {
+                x4 = x2 + x1 + x0;
+
+                x0 = x1;
+
+                x1 = x2;
+
+                x2 = x4;
+
+            }
+
+
+            Console.WriteLine(x4);
+
         }
     }
 }
